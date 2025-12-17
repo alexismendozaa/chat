5c268c2cdef74c0b6e0a3cf1e0c59784533547e2
alexismendozaa <afmendozaf@uce.edu.ec>
2025-12-17 00:18:32 -0500

    Add AWS credentials to deploy workflow and simplify image upload route

--- apps/backend/src/uploads/upload.routes.js
+++ apps/backend/src/uploads/upload.routes.js
@@ -1,14 +1,8 @@
 import { Router } from "express";
-import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import crypto from "crypto";
 import { requireAuth } from "../middlewares/auth.middleware.js";
 
 const r = Router();
-
-const s3 = new S3Client({
-  region: process.env.AWS_REGION,
-});
 
 r.post("/image", requireAuth, async (req, res) => {
   const { contentType } = req.body;
@@ -19,16 +13,8 @@ r.post("/image", requireAuth, async (req, res) => {
   const ext = contentType.split("/")[1];
   const key = `uploads/${crypto.randomUUID()}.${ext}`;
 
-  const command = new PutObjectCommand({
-    Bucket: process.env.S3_BUCKET,
-    Key: key,
-    ContentType: contentType,
-    ACL: "public-read",
-  });
-
-  const uploadUrl = await getSignedUrl(s3, command, { expiresIn: 60 });
-
-  const publicUrl = `https://${process.env.S3_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com/${key}`;
+  const uploadUrl = `https://${process.env.S3_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com/${key}`;
+  const publicUrl = uploadUrl;
 
   res.json({ uploadUrl, publicUrl });
 });
